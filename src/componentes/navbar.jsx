d2d7ef151b064ac5cff6c6466c0d0e74a0cc8cb8
Ezequiel Albornoz <eze_thomi@hotmail.com>
2024-07-08 14:59:57 -0300

    estilos nuevos en nav

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import "./navbar.css";
 import CartWidget from "./CartWidget";
+import { MdLaptopChromebook } from "react-icons/md";
+import { LuSmartphone } from "react-icons/lu";
+import { FaHeadphones } from "react-icons/fa";
+import { IoPeopleSharp } from "react-icons/io5";
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            <h1>Tecno Shop</h1>
+            <h1>
+                Tecno Shop{" "}
+                <span>
+                    <img
+                        src="https://cdn-icons-png.flaticon.com/512/552/552489.png"
+                        alt=""
+                        className="logo"
+                    />
+                </span>
+            </h1>
             <ul className="navbar-menu">
                 <li className="navbar-item">
-                    <a href="#home">Home</a>
+                    <a href="#home">
+                        <IoPeopleSharp />
+                    </a>
                 </li>
                 <li className="navbar-item">
-                    <a href="#about">About</a>
+                    <a href="#about">
+                        <MdLaptopChromebook />
+                    </a>
                 </li>
                 <li className="navbar-item">
-                    <a href="#services">Services</a>
+                    <a href="#services">
+                        <LuSmartphone />
+                    </a>
                 </li>
                 <li className="navbar-item">
-                    <a href="#contact">Contact</a>
+                    <a href="#contact">
+                        <FaHeadphones />
+                    </a>
                 </li>
                 <li className="navbar-item">
                     <CartWidget />
